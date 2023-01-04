9e203fe2a3e0d7885d548c1ff754b5b05af19765
DoctorSHIZA1 <113828723+DoctorSHIZA1@users.noreply.github.com>
2023-01-04 18:49:33 +0300

    Add files via upload

---
+++ dz2.py
@@ -0,0 +1,22 @@
+# задача 10
+
+# задача 12
+
+# while True:
+#     try:
+#         S = int(input("Введите сумму двух чисел: "))
+#         P = int(input("Введите произведение двух чисел: "))
+#         num1 = 0
+                
+#         while num1 <= S:
+#             num2 = S - num1
+#             if (num1 + num2 == S) and (num1 * num2 == P):
+#                 break
+#             else: num1 += 1
+
+#         print(f'Первое число -> {num1}, второе число -> {num2}')
+#         break
+#     except:
+#         print('Некорректный ввод. Попробуйте еще раз!')
+
+# задача 14
